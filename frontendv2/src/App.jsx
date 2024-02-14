76a7f06143fae6244fe0d225ed9b6482b147bf4e
Iboomnarak <warutboom100@gmail.com>
2024-02-14 22:47:07 +0700

    Update App.jsx

--- frontendv2/src/App.jsx
+++ frontendv2/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { lazy, useEffect } from 'react'
-import { BrowserRouter, Routes, Route ,Navigate} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import checkAuth from './routes/auth';
 
@@ -10,23 +10,19 @@ const Login = lazy(() => import('./pages/Login'))
 const token = checkAuth()
 
 export default function App() {
-  return(
+  return (
     <>
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/documentation" element={<Documentation />} />
-          
+        
           {/* Place new routes over this */}
           <Route path="/app/*" element={<Layout />} />
 
-          <Route path="*" element={<Navigate to={token ? "/app/welcome" : "/login"} replace />}/>
+          <Route path="*" element={<Navigate to={token ? "/app/dashboard" : "/login"} replace />} />
 
         </Routes>
       </Router>
     </>
-    
-  ) ;
+  );
 }
